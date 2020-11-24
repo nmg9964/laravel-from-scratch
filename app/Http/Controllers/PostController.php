9dc1d0739f1cb7e80d4670e8ce0c4d3d29f99b40
NaveedG <nganjani@protonmail.com>
2020-11-24 15:17:48 -0600

    fn readability

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,7 +7,7 @@ use App\Models\Post;
 
 class PostController extends Controller {
     public function show($title) {
-        $post = Post::where('title', $title)->firstorFail();
+        $post = Post::where('title', $title)->firstOrFail();
 
         return view('test', ['post' => $post]);
     }
