6406a484ab1ba4d05a5910fe01fe90dbe74d49d3
NaveedG <nganjani@protonmail.com>
2020-12-15 17:52:02 -0600

    create article layout

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -4,7 +4,37 @@
 <div id="wrapper">
 	<div id="page" class="container">
         <h1>New Article</h1>
+        <form>
+            <div class="field">
+                <label class="label" for="title">Title</label>
 
+                <div class="control">
+                    <input class="input" type="text" name="title">
+                </div>
+            </div>
+
+            <div class="field">
+                <label class="label" for="excerpt">Excerpt</label>
+
+                <div class="control">
+                    <textarea class="textarea" type="text" name="excerpt"></textarea>
+                </div>
+            </div>
+
+            <div class="field">
+                <label class="label" for="body">Body</label>
+
+                <div class="control">
+                    <textarea class="textarea" type="text" name="body"></textarea>
+                </div>
+            </div>
+
+            <div class="field is-grouped">
+                <div class="control">
+                    <button class="button" type="submit">Submit</button>
+                </div>
+            </div>
+        </form>
     </div>
 </div>
 
