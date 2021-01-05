1bea93225cdd778695545eba3c2e24ed9bda6bc6
NaveedG <nganjani@protonmail.com>
2021-01-04 18:33:18 -0600

    basic eloquent relationships

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     protected $fillable = ['title', 'excerpt', 'body'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
